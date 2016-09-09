6d8810c3ef2abee5605d9538b71652dbf8007909
OscarYuen <oscaryuen@gmail.com>
2016-09-09 23:40:01 +0800

    Update countryflags.directive.js
    clean code

--- src/countryflags.directive.js
+++ src/countryflags.directive.js
@@ -10,8 +10,8 @@
             restrict: 'E',
             transclude: true,
             template:
-                    '<span ng-if="scope.isSquare" class="flag-icon flag-icon-{{ country }} flag-icon-square"></span>' +
-                    '<span ng-if="!scope.isSquare" class="flag-icon flag-icon-{{ country }}"></span>' +
+                    '<span ng-if="isSquare" class="flag-icon flag-icon-{{ country }} flag-icon-square"></span>' +
+                    '<span ng-if="!isSquare" class="flag-icon flag-icon-{{ country }}"></span>' +
                     '<ng-transclude></ng-transclude>',
             scope: {
                 country: '@',
@@ -31,7 +31,7 @@
                 }
             });
 
-            scope.$watch('isSquare', function(value) {
+            attrs.$observe('isSquare', function(value) {
               if(value != null){
                  scope.isSquare = value;
               }
